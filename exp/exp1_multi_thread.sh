b1fa0c110f1a449d4c92e73f56b61a7ad49bd7d1
谢光桓 <xgh536@qq.com>
2022-03-09 18:56:05 +0800

    exp changes

--- exp/exp1_multi_thread.sh
+++ exp/exp1_multi_thread.sh
@@ -1,3 +1,6 @@
-killall sm_manager gg18_keygen_client_queue
-./target/debug/examples/sm_manager &
-./target/debug/examples/gg18_keygen_client_multi_threads http://127.0.0.1:8001 $1 &
+killall sm_manager gg18_keygen_client_multi_threads
+./target/debug/examples/sm_manager >> /dev/null &
+./target/debug/examples/gg18_keygen_client_multi_threads http://127.0.0.1:8001 $1  >> /dev/null &
+pid=$(pidof gg18_keygen_client_multi_threads)
+top -n 7 -p $pid -d 5 -b
+killall sm_manager gg18_keygen_client_multi_threads
