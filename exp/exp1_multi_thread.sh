530b6239a9b9c569ff98c1f978d68ea98b5e103a
谢光桓 <xgh536@qq.com>
2022-03-09 18:34:29 +0800

    exp shell

---
+++ exp/exp1_multi_thread.sh
@@ -0,0 +1,3 @@
+killall sm_manager gg18_keygen_client_queue
+./target/debug/examples/sm_manager &
+./target/debug/examples/gg18_keygen_client_multi_threads http://127.0.0.1:8001 $1 &
---
+++ exp/exp1_queue.sh
@@ -0,0 +1,3 @@
+killall sm_manager gg18_keygen_client_queue
+./target/debug/examples/sm_manager &
+./target/debug/examples/gg18_keygen_client_queue http://127.0.0.1:8001 $1 &
